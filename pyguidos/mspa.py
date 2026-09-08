4b9abd1220caa538f1177078f653f452ddfbcac2
Giovanni Caudullo <giovanni.caudullo@ext.ec.europa.eu>
2026-09-08 09:46:15 +0200

    fix miallib bug1

--- pyguidos/mspa.py
+++ pyguidos/mspa.py
@@ -145,7 +145,7 @@ def mspa(in_tiff,
                                    trans_i,
                                    intext_i)
         # Post-process to fix miallib bug: if pixels = 2 appear, they are converted to Background
-        mspa_array[mspa_array=2] = 0
+        mspa_array[mspa_array == 2] = 0
         
         # Save Final GeoTIFF with palette and tags. 
         weblink = "https://forest.jrc.ec.europa.eu/en/activities/lpa/mspa/"
